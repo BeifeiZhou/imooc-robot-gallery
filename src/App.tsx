403e9830d97c3daa71c4b9fbf53336129b4a8d6c
Beifei Zhou <z.beifei@gmail.com>
2021-09-24 11:45:08 +0100

    Chapter 3 class component

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,59 @@ import logo from './assets/images/logo.svg';
 import styles from './App.module.css';
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
+import ShoppingCart from './components/ShoppingCart'
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <h1>Robot online shopping</h1>
+interface Props { }
+interface State {
+  robotGallery: any[];
+  count: number
+}
+class App extends React.Component<Props, State> {
+
+  // 生命周期第一阶段： 初始化
+  constructor(props) {
+    super(props);
+    this.state = {
+      robotGallery: [],
+      count: 0
+    }
+  }
+
+  // 在组建初始化的时候调用一次
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => this.setState({ robotGallery: data }))
+  }
+
+  // 组建更新后调用
+  componentDidUpdate() { }
+
+  // 组建销毁后调用
+  componentWillUnmount() { }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>Robot online shopping</h1>
+        </div>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 }, () => {
+              console.log("count ", this.state.count)
+            })
+          }}
+        >Click</button>
+        <span>count: {this.state.count}</span>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
+        </div>
       </div>
-      <div className={styles.robotList}>
-        {robots.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styles from './ShoppingCart.module.css'
+import styles from './ShoppingCart.modeule.css'
+import { FiShoppingCart } from 'react-icons/fi'
 
 interface Props { }
 
@@ -13,7 +14,36 @@ class ShoppingCart extends React.Component<Props, State> {
         }
     }
 
+    click = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        if ((e.target as HTMLElement).nodeName === "SPAN") {
+            this.setState({ isOpen: !this.state.isOpen })
+        }
+    }
+
     render() {
-        return <div></div>
+        return <div className={styles.cardContainer}>
+            <button
+                className={styles.button}
+                onClick={this.click}
+            >
+                <FiShoppingCart />
+                <span>
+                    购物车2件
+                </span>
+            </button>
+            <div
+                className={styles.cartDropDown}
+                style={{
+                    display: this.state.isOpen ? "block" : "none",
+                }}
+            >
+                <ul>
+                    <li>robot 1</li>
+                    <li>robot 2</li>
+                </ul>
+            </div>
+        </div>
     }
 }
+
+export default ShoppingCart;
